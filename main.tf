42c4abb92681d6abcc735759fc2936840a9a949b
Simon Duff <simon.duff@gmail.com>
2022-04-06 12:56:47 +1000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+# Terraform Provider for Confluent Cloud
+#
+terraform {
+   required_providers {
+      confluentcloud = {
+         source  = "confluentinc/confluentcloud"
+         version = "0.5.0"
+      }
+   }
+}
+
+provider "confluentcloud" {}
+
+# The environment we use is stored in Terraform Cloud Variables
+data "confluentcloud_environment" "shared-env" {
+   id = "${var.CONFLUENT_CLOUD_ENVIRONMENT_ID}"
+}
+
+# Create a basic cluster
+resource "confluentcloud_kafka_cluster" "basic-cluster" {
+   display_name = "sduff_basic_kafka_cluster"
+   availability = "SINGLE_ZONE"
+   cloud        = "AWS"
+   region       = "ap-southeast-2"
+   basic {}
+
+   environment {
+      id = data.confluentcloud_environment.shared-env.id
+   }
+}
